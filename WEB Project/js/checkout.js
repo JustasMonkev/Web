ceb392b6a743a5f7032d1986302568ebc6353a2e
JustasMonkev <justas.monkev@gmail.com>
2020-03-22 19:55:58 +0200

    Add files via upload

--- WEB Project/js/checkout.js
+++
@@ -1,6 +0,0 @@
-        var form = document.getElementById("form2");
-        form.addEventListener("submit", function(e) {
-            e.preventDefault();
-            location.replace("../index.html");
-            alert("thank for your order");
-        });
